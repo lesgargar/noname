d495963ec64b720bd50410872299fd25ca64c745
lesgargar <ggarciaveka@gmail.com>
2023-07-19 19:08:30 -0500

    connected and collections done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Auth } from './components/Auth';
+import {db} from './config/firebase';
+import {getDocs, collection} from 'firebase/firestore';
 
 function App() {
+
+  const[movielist, setMovielist] = useState([]);
+  const moviesCollectionRef = collection(db, "movies")
+//getmovielist funct shows list of movies 
+  useEffect(()=>{
+    const getMovielist = async () =>{
+      try{
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map(
+          (doc)=>(
+            {...doc.data(),
+               id:doc.id
+              }));
+              setMovielist(filteredData)
+      }catch(err){
+        console.log(err)
+      }
+    };
+    getMovielist();
+  },[])
+  
   return (
     <div className="App">
      <Auth/>
-     
+     <div>
+      {movielist.map((movie)=>(
+        <div>
+          <h1> {movie.title} </h1>
+        </div>
+      ))}
+     </div>
+    
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBmqXTO0Q_Cc8wYHrLV_ov7dhnVNLW8Mng",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider(); 
+export const db = getFirestore(app);
